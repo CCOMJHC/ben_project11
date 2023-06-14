5f8e103adddd43c7430400aa5b10f7e7ee47a02a
Field User <field@ccom.unh.edu>
2023-06-14 16:24:49 +0000

    Enable dualcamp for startup script

--- scripts/start_tmux_operator_ui.bash
+++ scripts/start_tmux_operator_ui.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tmux new -d -s project11-camp
-tmux send-keys "rosrun rosmon rosmon --name=rosmon_operator_ui ben_project11 operator_ui.launch" C-m
+tmux send-keys "rosrun rosmon rosmon --name=rosmon_operator_ui ben_project11 operator_ui.launch dual_camp:=true" C-m
 tmux a -t project11-camp
